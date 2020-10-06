b0c4083d840628b8cfa7fafd295129d3717c7acc
mari-shakhanava <maria.shakhanava@gmail.com>
2020-10-06 17:16:07 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
-}
+  var zeroToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+      'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+      'seventeen', 'eighteen', 'nineteen'];
+  var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+      'ninety'];
+  var stringOfGivenNumber = number.toString();
+
+
+  if (number <20){
+      return zeroToNineteen[number];
+  }
+
+  if (stringOfGivenNumber.length === 2){
+      if (stringOfGivenNumber[1] === '0'){
+          return  tens[stringOfGivenNumber[0]];
+      } else
+      return tens[stringOfGivenNumber[0]]+' '+zeroToNineteen[stringOfGivenNumber[1]];
+  }
+
+  if (stringOfGivenNumber.length === 3){
+      if (stringOfGivenNumber[1] === '0' && stringOfGivenNumber[2] === '0' ) {
+          return zeroToNineteen[stringOfGivenNumber[0]] + ' hundred';
+      } else
+
+      return zeroToNineteen[stringOfGivenNumber[0]]+' hundred '+ toReadable(+(stringOfGivenNumber[1]+stringOfGivenNumber[2]));
+
+      }
+  }
+
+
